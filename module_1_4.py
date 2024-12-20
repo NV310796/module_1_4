0081900aa1449ad3b8f3e769dcf905819707ccd0
NV310796 <geniynashihdney@gmail.com>
2024-12-20 22:12:20 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input('Произвольный текст: ')
+print('Колличество символов в произвольном тексте:', len(my_string))
+print('Верхний регистр:', my_string.upper())
+print('Нижний регистр:', my_string.lower())
+print('Без пробелов:', my_string.replace(' ', ''))
+print('Первый символ:', my_string[0])
+print('Последний символ:', my_string[-1])
+import this
